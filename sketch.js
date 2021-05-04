e4a58f81a0d8fbfac57a7209e089e49eb2fb28f3
Anant <anantaravind@outlook.com>
2021-05-04 15:50:08 +0530

    changed the background

--- sketch.js
+++ sketch.js
@@ -1,3 +1,11 @@
+var aart1 = [1,2,3,4,5];
+console.log(aart1);
+console.log(aart1[2]);
+var aart2 = [[1,2],[3,4],[5,6]]
+console.log(aart2);
+console.log(aart2[1][0]);
+
+
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
@@ -7,10 +15,12 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
-
+var gameState = "onSling";
+var bg = "sprites/bg.png";
+var score = 0;
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+getBackgroundImg();
 }
 
 function setup(){
@@ -41,21 +51,29 @@ function setup(){
 
     //log6 = new Log(230,180,80, PI/2);
     slingshot = new SlingShot(bird.body,{x:200, y:50});
+
 }
 
 function draw(){
+    if(backgroundImg)
     background(backgroundImg);
+    noStroke()
+    textSize(35);
+    fill(255);
+    text("Score "+score,width-300,50)
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -69,16 +87,37 @@ function draw(){
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    if(gameState!== "launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
+   
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gameState = "launched";
 }
 
 function keyPressed(){
     if(keyCode === 32){
-        slingshot.attach(bird.body);
+       // slingshot.attach(bird.body);
     }
+    
+}
+async function getBackgroundImg(){
+    var response =  await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
+var responseJSON = await response.json();
+console.log(responseJSON);
+var dateTime = responseJSON.datetime;
+console.log(dateTime);
+var hour  = dateTime.slice(11,13);
+console.log(hour);
+if (hour>=06 && hour<=19){
+bg = "sprites/bg.png"
+}
+else{
+    bg = "sprites/bg2.jpg"
+}
+backgroundImg = loadImage(bg);
 }
